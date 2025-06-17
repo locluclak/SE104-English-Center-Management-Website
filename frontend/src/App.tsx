0e8e72c53480835fd9b2323ae2cf1b0b2c05e992
NguyenCongHau25 <154607553+NguyenCongHau25@users.noreply.github.com>
2025-06-17 23:34:44 +0700

    delete all file

--- frontend/src/App.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import MainRoutes from "./routes/MainRoutes";
-
-export default function App() {
-  return <MainRoutes />;
-}
--- frontend/src/utils/uri.ts
+++
@@ -1,5 +0,0 @@
-export const comparePathname = (pathname1: string, pathname2: string) => {
-    const path1 = pathname1.replace(/(^\/)|(\/$)/g, '');
-    const path2 = pathname2.replace(/(^\/)|(\/$)/g, '');
-    return path1 === path2;
-}
